07fc2bc65328128ad210f8930a2ca439bfc51b5f
Administrador <Administrador@10.10.4.13>
2019-07-30 19:44:53 -0300

    chorando

---
+++ Ator/src/dao/AtorDao.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Ator;
+/**
+ *
+ * @author Administrador
+ */
+public class AtorDao {
+    public static boolean inserir(Ator objeto) {
+        String sql = "INSERT INTO Ator (codigo, nome_real, nome_artistico) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getSigla());
+            ps.setString(2, objeto.getNome());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+}
